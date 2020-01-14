88f9330f189d63db6d98e0f3944760655bfc95b9
mt <margarettom6@gmail.com>
2020-01-14 09:20:59 +0300

    resposniveness

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-      <!-- <div class="container">
+    <!-- <div class="container">
       <div class="links">
         <h1></h1>
         <nuxt-link to="/courses">Go to courses page</nuxt-link>
       </div>
-    </div> -->
+    </div>-->
 
     <!-- All elements enclosed -->
     <div class="container">
@@ -18,8 +18,6 @@
       </div>
     </div>
   </div>
-
-  
 </template>
 
 <script>
@@ -38,42 +36,59 @@ export default {
   text-align: center;
 } */
 
-  .container {
-        position: absolute;
+.container {
+  position: absolute;
+
+  /* top: 0; */
+  /* width: 100%; */
+  height: 93%;
+  padding-left: 0;
+}
+.background {
+  /* position: relative; */
+  height: 100%;
+  width: 100vw;
+  left: 0;
+  background: linear-gradient(rgba(5, 13, 48, 0.25), rgba(5, 13, 48, 0.25)),
+    url('../assets/online-4668930_1920.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+  /* background-position: center center; */
+}
+.content {
+  position: absolute;
+  bottom: 0;
+  /* background-color: rgb(200, 200, 224); */
+  background-color: rgb(180, 180, 184);
+  color: blue !important;
+  width: 100vw;
+  text-align: center;
+  opacity: 0.9;
+}
+.title {
+  font-size: 4em;
+}
+.sub-title {
+  /* margin-top: -2rem;  */
+  font-size: 1.5rem;
+}
+
+@media screen and (max-width: 768px) {
+.container{
+  height: 95vh;
+}
+  .background {
+  background-size: contain;
+
+  }
+  .content{
+  
+  bottom: 2rem;
+  background-color: unset;
     
-        /* top: 0; */
-        /* width: 100%; */
-        height: 100%;
-        padding-left: 0;
-      }
-      .background{
-        /* position: relative; */
-        height: 100%;
-        width: 100vw;
-            left: 0;
-        background: linear-gradient(rgba(5, 13, 48, 0.25),rgba(5, 13, 48, 0.25)), url("../assets/online-4668930_1920.jpg");
-        background-size: cover;
-        background-repeat: no-repeat;
-        display: flex;
-        /* background-position: center center; */
-      }
-      .content {
-        
-        position: absolute;
-        bottom: 0;
-        /* background-color: rgb(200, 200, 224); */
-        background-color: rgb(180, 180, 184);
-        color: blue !important;
-        height: 25%;
-        width:100vw;
-        text-align:center;
-        opacity: 0.9;
-      }
-      .title{
-          font-size: 4em;
-      }
-      .sub-title{
-          /* margin-top: -2rem;  */
-          font-size: 1.5rem;
-      }
+  }
+  
+
+}
 </style>
